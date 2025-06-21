5ed02e2f00f64d00d562e52836bd00f372c0a9b9
TahaTesser <48603081+TahaTesser@users.noreply.github.com>
2025-06-21 06:26:00 +0000

    Deploying to gh-pages from @ TahaTesser/designdino@75d8ff861ac2bf26c887d75486a700827be42a44 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1936364557"
+    serviceWorkerVersion: "1928441340"
   }
 });
